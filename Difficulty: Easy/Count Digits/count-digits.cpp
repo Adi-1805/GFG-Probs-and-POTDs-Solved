198747fc2d8e5c910445dd3a6092c446199fca79
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2024-08-13 12:23:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int count = 0;
+        int temp = N*10;
+        while(temp){
+            temp /= 10;
+            if(temp%10 == 0) continue;
+            else count += (N%(temp%10) == 0);
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
