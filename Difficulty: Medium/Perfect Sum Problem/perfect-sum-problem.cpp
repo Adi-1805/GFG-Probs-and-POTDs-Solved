067ccb2f083d73864e398710a487d2ae228f0e62
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-16 01:03:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>> dp;
+    int solve(vector<int>& arr, int ind, int target){
+        if(ind == 0){
+            if(target == 0) return 1 + (target == arr[ind]);
+            return (target == arr[ind]);
+        } 
+        if(dp[ind][target] != -1) return dp[ind][target];
+        
+        int not_take = solve(arr, ind-1, target);
+        int take = 0;
+        if(arr[ind] <= target) take = solve(arr, ind-1, target - arr[ind]);
+        
+        return dp[ind][target] = take + not_take;
+    }
+  public:
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        dp.resize(n, vector<int>(target+1, -1));
+        return solve(arr, n-1, target);
+    }
+};
