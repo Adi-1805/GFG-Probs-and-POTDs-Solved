c1624b79d79382aa315ffffac1e8a3e59dfcf4c5
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-18 20:36:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int perfectSum(vector<int>& arr, int target) {
         int n = arr.size();
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        int zeroes = 0;
+        for(int val: arr) if(val == 0) zeroes++;
+        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
         
-        dp[0][0] = 1;
+        for(int i = 0; i < n; i++) dp[i][0] = 1;
+        if(arr[0] <= target) dp[0][arr[0]] = 1;
         
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j <= target; ++j) {
-                dp[i][j] = dp[i-1][j];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j <= target; ++j) {
+                int not_take = dp[i-1][j], take = 0;
                 
-                if (arr[i-1] <= j) {
-                    dp[i][j] += dp[i-1][j - arr[i-1]];
+                if (arr[i] <= j and arr[i] != 0) {
+                    take = dp[i-1][j - arr[i]];
                 }
+                dp[i][j] = take+not_take;
             }
         }
         
-        return dp[n][target];
+        return dp[n-1][target]*pow(2, zeroes);
     }
 };
