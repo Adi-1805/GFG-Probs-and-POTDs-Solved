bb8aa63241c2986df335a30e682b7728a8de36ba
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-07-22 21:40:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,22 +1,22 @@
 class Solution {
-    vector<vector<int>> dp;
-    int solve(int ind, int wt, vector<int> &arr, vector<int> &weight){
-        if(ind == 0){
-            if(weight[ind] <= wt) return arr[ind];
-            else return 0;
-        }
-        if(dp[ind][wt] != -1) return dp[ind][wt];
-        
-        int not_take = solve(ind-1, wt, arr, weight);
-        int take = INT_MIN;
-        if(weight[ind] <= wt) take = arr[ind] + solve(ind-1, wt - weight[ind], arr, weight);
-        return dp[ind][wt] = max(not_take, take);
-    }
   public:
-    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+    int knapsack(int W, vector<int> &val, vector<int> &weight) {
         // code here
         int n = val.size();
-        dp.resize(n, vector<int>(W+1, -1));
-        return solve(n-1, W, val, wt);
+        vector<vector<int>> dp(n, vector<int>(W+1, 0));
+        for(int wt = 0; wt <= W; wt++){
+            if(weight[0] <= wt) dp[0][wt] = val[0];
+            else dp[0][wt] = 0;
+        }
+        for(int ind = 1; ind < n; ind++){
+            for(int wt = 0; wt <= W; wt++){
+                int not_take = dp[ind-1][wt];
+                int take = INT_MIN;
+                if(weight[ind] <= wt) take = val[ind] + dp[ind-1][wt - weight[ind]];
+                
+                dp[ind][wt] = max(not_take, take);
+            }
+        }
+        return dp[n-1][W];
     }
 };
