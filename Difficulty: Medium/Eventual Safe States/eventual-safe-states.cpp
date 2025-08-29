74e128852cd8a16fa25bd1da480e04481ce1d326
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2025-08-29 22:12:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Eventual Safe States/eventual-safe-states.cpp
@@ -0,0 +1,41 @@
+// User function Template for C++
+
+class Solution {
+    /* KAHN'S ALGORITHM */
+    void KahnAlgo(int V, vector<vector<int>>& adj, vector<int>& topoSort){
+        queue<int> q; 
+        vector<int> indegree(V, 0);
+        for(int i = 0; i < V; i++){
+            for(int verti: adj[i]) indegree[verti]++;
+        }
+        
+        for(int v = 0; v < V; v++) if(indegree[v] == 0) q.push(v);
+        
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            topoSort.push_back(node);
+            for(int next: adj[node]){
+                indegree[next]--;
+                if(indegree[next] == 0) q.push(next);
+            }
+        }
+    }
+    
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        // code here
+        vector<int> topoSort;
+        vector<vector<int>> revAdj(V);
+        for(int i = 0; i < V; i++){
+            for(int verti: adj[i]){
+                revAdj[verti].push_back(i);
+            }
+        }
+        
+        KahnAlgo(V, revAdj, topoSort);
+        sort(topoSort.begin(), topoSort.end());
+        return topoSort;
+        
+    }
+};
