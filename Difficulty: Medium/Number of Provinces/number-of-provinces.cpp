8d88f422cbd692584a558488603629acb5631568
Aditi Mishra <145211183+Adi-1805@users.noreply.github.com>
2026-03-17 21:41:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.cpp
@@ -1,65 +1,33 @@
 // User function Template for C++
-class DisjointSet {
-    vector<int> parent, size, rank;
-    public:
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); size.resize(n+1, 1);
-        parent.resize(n+1);
-        for(int i = 0; i < n; i++) parent[i] = i;
-    }
-    int findUltPar(int node){
-        if(parent[node] == node) return node;
-        return parent[node] = findUltPar(parent[node]);
-    }
-    void unionBySize(int u, int v){
-        int pu = findUltPar(u);
-        int pv = findUltPar(v);
-        if(pu == pv) return;
-        // Compare size of parents
-        if(size[pu] > size[pv]){ 
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        } else { 
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
-    }
-    void unionByRank(int u, int v){
-        int pu = findUltPar(u);
-        int pv = findUltPar(v);
-        if(pu == pv) return;
-        // Compare size of parents
-        if(rank[pu] > rank[pv]){ 
-            parent[pv] = pu;
-        } else if(rank[pu] < rank[pv]) { 
-            parent[pu] = pv;
-        } else {
-            parent[pu] = pv;
-            rank[pv] += 1;
-        }
-    }
-    
-};
 
 class Solution {
+    void dfs(int node, vector<vector<int>> &adj, vector<int> &vis){
+        vis[node] = 1;
+        for(int nbr : adj[node]){
+            if(!vis[nbr]) dfs(nbr, adj, vis);
+        }
+    }
   public:
     int numProvinces(vector<vector<int>> adj, int V) {
         // code here
-        int m = adj.size(), n = adj[0].size();
-        DisjointSet ds(V);
-        // vector<pair<int, int>> edges;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(adj[i][j]){
-                    // edges.push_back({i , j});
-                    ds.unionBySize(i, j);
+        vector<vector<int>> graph(V);
+        for(int i = 0; i < adj.size(); i++){
+            for(int j = 0; j < adj[0].size(); j++){
+                if(adj[i][j] == 1){
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
                 }
             }
         }
-        int ans = 0;
+        
+        vector<int> vis(V, 0);
+        int components = 0;
         for(int i = 0; i < V; i++){
-            if(ds.findUltPar(i) == i) ans++;
+            if(!vis[i]){
+                dfs(i, graph, vis);
+                components++;
+            } 
         }
-        return ans;
+        return components;
     }
 };
